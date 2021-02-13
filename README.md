4462056f07148f81efd64a857b5b10837ab9ffb0
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-02-13 17:55:14 +0530

    ADD: version 1.0.0 active

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # Thesaurus
-This is a thesaurus based on NodeJS made using datamuse API.
+
+> This is a thesaurus based on NodeJS made using datamuse API.
+
+## Table of contents
+
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+
+This project is about using the command line to make a thesaurus with different utilities like finding synonyms, antonyms, rhyming words and so much more. I decided to make this project because I wanted to understand the true power asynchronous model of NodeJS and JavaScript in general.
+
+## Screenshots
+
+1. basic usage
+![basic use](./resources/images/output-detailed.png)
+
+2. other option --antonym or -a
+![Antonym output](./resources/images/output-antonym.png)
+
+3. accessing help using --help or -h
+![help output](./resources/images/output-help.png)
+
+## Technologies
+
+* Node - version 14.15.0
+* Git and Github
+* Terminal
+* Yargs package
+* Node Fetch package
+
+## Setup
+
+1. Download and install NodeJS.
+2. Clone this repository.
+3. Use `npm install` to install the required packages. 
+4. Execute the main.js file using appropriate argument and flags.
+5. Use --help option to find out more about the usage.
+
+## Features
+
+List of features ready and TODOs for future development
+
+* Find word Synonyms
+* Find word Antonyms
+* Find similar sounding words
+* Find similar spelled words
+* Find Rhyming words
+
+To-do list:
+
+* Find other features like finding general form of a word.
+
+## Status
+
+Project is: _finished_
+
+Version 1.0.0 is active. More features will be added soon.
+
+## Inspiration
+
+I wanted to learn by doing. Learn the async aspects of node and put them to use. Learn promises, utilize them and understand them. I also wanted to write shell scripts using JavaScript by making use of NodeJS.
+
+## Contact
+
+Created by [@Goku-kun](https://github.com/Goku-kun) - feel free to contact me!
---
+++ main.js
@@ -0,0 +1,157 @@
+#! /usr/bin/env node
+
+"use strict";
+
+const fetch = require('node-fetch');
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage(
+        `
+Usage - 1(POSIX based env):        ./$0 [option] -w WORD
+
+Usage - 2(Windows based env):      node $0 [option] -w WORD
+
+
+argument meanings:
+
+[-w WORD] or [--word WORD] has WORD to be replaced with the word you want to access this thesaurus with.
+[option] argument has many options.
+`
+    )
+    .options({
+        'w': {
+            alias: 'word',
+            demandOption: true,
+            type: 'string',
+            describe: 'Option for the word to be entered to search the thesaurus'
+        },
+        's': {
+            alias: 'synonym',
+            demandOption: false,
+            type: 'boolean',
+            describe: 'Option to find the word synonyms'
+        },
+        'a': {
+            alias: 'antonym',
+            demandOption: false,
+            type: 'boolean',
+            describe: 'Option to find the word antonyms'
+        },
+        'o': {
+            alias: 'sound',
+            demandOption: false,
+            type: 'boolean',
+            describe: 'Option to find the similar sounding words'
+        },
+        'p': {
+            alias: 'spell',
+            demandOption: false,
+            type: 'boolean',
+            describe: 'Option to find the words with similar spellings'
+        },
+        'r': {
+            alias: 'rhyme',
+            demandOption: false,
+            type: 'boolean',
+            describe: 'Option to find the rhyming words'
+        },
+        'm': {
+            alias: 'maximum',
+            demandOption: false,
+            default: 10,
+            type: 'number',
+            describe: `Option to limit the maximum number of words in output. 
+            If more than 50, defaults to 10.`
+        },
+
+    })
+    .example('(POSIX env):          $ ./main.js --syn -w hope')
+    .example('(Windows env):        > node main.js --syn -w hope')
+    .version('1.0.0')
+    .argv
+
+// Limit the number of output fetched and returned
+if (argv.m > 50) {
+    argv.m = 10;
+}
+
+const url = `https://api.datamuse.com/words?&max=${argv.m}`
+
+
+
+if (argv.s) {
+    findSynonyms();
+} else if (argv.a) {
+    findAntonyms();
+} else if (argv.o) {
+    findSimilarSounds();
+} else if (argv.p) {
+    findSimilarSpells();
+} else if (argv.r) {
+    findRhymingWords();
+} else {
+    error('Improper usage of thesaurus', true)
+}
+
+/***************************** */
+
+function fetcher(queryString) {
+    // TODO: add logic to fetch the query string
+    const finalSearchString = url + queryString;
+    fetch(finalSearchString)
+        .then(res => res.json())
+        .then(displayWords)
+        .catch(error)
+}
+
+function findSynonyms() {
+    const queryString = `&rel_syn=${argv.w}`
+    fetcher(queryString)
+}
+
+function findAntonyms() {
+    // TODO: add logic to find word antonyms
+    const queryString = `&rel_ant=${argv.w}`
+    fetcher(queryString)
+}
+
+function findSimilarSounds(word) {
+    // TODO: add logic to find similar sounding words
+    const queryString = `&sl=${argv.w}`
+    fetcher(queryString)
+
+}
+
+function findSimilarSpells(word) {
+    // TODO: add logic to find similar spelled words
+    const queryString = `&sp=${argv.w}`
+    fetcher(queryString)
+}
+
+function findRhymingWords(word) {
+    // TODO: add logic to find Rhyming words
+    const queryString = `&rel_rhy=${argv.w}`
+    fetcher(queryString)
+}
+
+function displayWords(array) {
+    console.log(`
+ ___________________
+|                   |
+|*******************|
+|      OUTPUT       |
+|*******************|
+|___________________|
+
+    `);
+    for (let word of array) {
+        console.log(`▶  ${word.word}`)
+    }
+}
+
+function error(msg, includeHelp = false) {
+    console.error(msg);
+    if (includeHelp) {
+        console.log();
+        console.log('print help using --help option to learn more about usage');
+    }
+}
